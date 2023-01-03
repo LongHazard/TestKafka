535af50a002d6c4f6b15c2800a237ad593629505
LongHazard <nguyenlongvan1997@gmail.com>
2023-01-03 12:06:38 +0700

    get data kafka

---
+++ src/main/java/com/example/KafkaExample/KafkaExampleApplication.java
@@ -0,0 +1,16 @@
+package com.example.KafkaExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+
+@SpringBootApplication
+public class KafkaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaExampleApplication.class, args);
+	}
+
+
+}
